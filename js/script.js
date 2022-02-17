9882692c4c43af01f0540f1f421a38f36e070f74
Josue-FDev <josuefallasm26@hotmail.com>
2022-02-17 08:16:23 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-var o = document.getElementById('org');
+var i = document.getElementById('info');
 // var m = $('#motivo');
-var org = document.getElementById('info-org');
-o.onclick = () => {
-    org.style.display = "block";
-    nacimiento.style.display = "none";
-    territorio.style.display = "none";
-    motivo.style.display = "none";
+var box = document.getElementById('box-text');
+i.onclick = () => {
+    box.style.display = "block";
 };
 
