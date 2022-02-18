7bc022e9b4055f4680e2a97753a04e82046a34ab
Josue-FDev <josuefallasm26@hotmail.com>
2022-02-18 21:37:36 +0000

    update

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ var i = document.getElementById('info');
 var box = document.getElementById('box-text');
 i.onclick = () => {
     box.style.display = "block";
+    i.style.display = "none";
 };
+var mark = document.getElementById('mark');
+mark.onclick = () => {
+    box.style.display = "none";
+    i.style.display = "block";
+}
 
